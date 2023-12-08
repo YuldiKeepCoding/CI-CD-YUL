5da9a68525be81c70ab9ec1481476d1484f13a6b
Yuldi <yuldy88@gmail.com>
2023-12-08 17:07:24 +0000

    Initial CICD

---
+++ app/main.py
@@ -0,0 +1,88 @@
+"""
+Este script define una calculadora simple y muestra un arte ASCII de bienvenida.
+La calculadora tiene operaciones de suma, resta, multiplicación y división.
+"""
+print('''
+'    ####      ##    ####    ####  #### ###  ####      ##    ######    ####   ######      ##'
+'   ##  ##     ##     ##    ##  ##  ##  ##    ##       ##     ##  ##  ##  ##   ##  #      ##'
+'  ###        ###     ##   ###      ##  ##    ##      ###     ##  ## ###  ##   ## ##     ###'
+'  ##        #  ##   ##    ##      ##  ##    ##      #  ##   ##   ## ##   ##   ####     #  ##'
+'  ##        #####   ##    ##      ##  ##    ##      #####   ##  ### ##  ###  ## ##     #####'
+'  ##   #   #   ##   ##  # ##   #  ##  ##    ##  #  #   ##   ##  ##  ##  ##   ##  ##   #   ##'
+'   ####   ### #### ######  ####    ####    ###### ### #### ######    ####   #### ### ### ####'
+
+
+ _____________________
+|  _________________  |
+| |   Calculadora   | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | * | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |   
+|_____________________|
+
+''')
+class Calculadora:
+    """
+    Clase que implementa una calculadora con operaciones básicas.
+    """
+
+    def add(self, num1, num2):
+        """
+        Realiza la operación de suma.
+
+        Args:
+            num1 (int): Primer número.
+            num2 (int): Segundo número.
+
+        Returns:
+            int: Resultado de la suma.
+        """
+        return num1 + num2
+
+    def subtract(self, num1, num2):
+        """
+        Realiza la operación de resta.
+
+        Args:
+            num1 (int): Número del que se resta.
+            num2 (int): Número que se resta.
+
+        Returns:
+            int: Resultado de la resta.
+        """
+        return num1 - num2
+
+    def multiply(self, num1, num2):
+        """
+        Realiza la operación de multiplicación.
+
+        Args:
+            num1 (int): Primer número.
+            num2 (int): Segundo número.
+
+        Returns:
+            int: Resultado de la multiplicación.
+        """
+        return num1 * num2
+
+    def divide(self, num1, num2):
+        """
+        Realiza la operación de división.
+
+        Args:
+            num1 (int): Número dividendo.
+            num2 (int): Número divisor.
+
+        Returns:
+            float or str: Resultado de la división o mensaje de error si el divisor es 0.
+        """
+        if num2 == 0:
+            return 'Cannot divide by 0'
+        return num1 * 1.0 / num2
---
+++ app/test.py
@@ -0,0 +1,42 @@
+from main import Calculadora
+
+def test_add():
+    """
+    Pruebas unitarias para el método add de la clase Calculadora.
+    """
+    assert Calculadora().add(1, 2) == 3.0
+    assert Calculadora().add(1.0, 2.0) == 3.0
+    assert Calculadora().add(0, 2.0) == 2.0
+    assert Calculadora().add(2.0, 0) == 2.0
+    assert Calculadora().add(-4, 2.0) == -2.0
+
+def test_subtract():
+    """
+    Pruebas unitarias para el método subtract de la clase Calculadora.
+    """
+    assert Calculadora().subtract(1, 2) == -1.0
+    assert Calculadora().subtract(2, 1) == 1.0
+    assert Calculadora().subtract(1.0, 2.0) == -1.0
+    assert Calculadora().subtract(0, 2.0) == -2.0
+    assert Calculadora().subtract(2.0, 0.0) == 2.0
+    assert Calculadora().subtract(-4, 2.0) == -6.0
+
+def test_multiply():
+    """
+    Pruebas unitarias para el método multiply de la clase Calculadora.
+    """
+    assert Calculadora().multiply(1, 2) == 2.0
+    assert Calculadora().multiply(1.0, 2.0) == 2.0
+    assert Calculadora().multiply(0, 2.0) == 0.0
+    assert Calculadora().multiply(2.0, 0.0) == 0.0
+    assert Calculadora().multiply(-4, 2.0) == -8.0
+
+def test_divide():
+    """
+    Pruebas unitarias para el método divide de la clase Calculadora.
+    """
+    # assert Calculadora().divide(1, 2) == 0.5
+    assert Calculadora().divide(1.0, 2.0) == 0.5
+    assert Calculadora().divide(0, 2.0) == 0
+    assert Calculadora().divide(-4, 2.0) == -2.0
+    # assert Calculadora().divide(2.0, 0.0) == 'Cannot divide by 0'
